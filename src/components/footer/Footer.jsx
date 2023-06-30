51f0a1c8eb6d777f00a7aa8beefb161d13a0b1f4
carlosHLeite <carlosleiteh@gmail.com>
2023-06-29 23:52:56 -0300

    Css modules added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+
+const Footer = () => {
+
+  console.log("Footerrrr again")
+  return (
+    <div>
+        <div>©2023 Lamamia. All rights reserved.</div>
+    </div>
+  )
+}
+
+export default Footer
